e86cb6fa14a6f2388b42fa18ae58aae456111b4b
Misty94 <mms.strickland@gmail.com>
2022-06-27 19:58:00 -0400

    button clicker

---
+++ Week2Day1/Strickland_Misty_ButtonClicker/script.js
@@ -0,0 +1,19 @@
+
+function logout(element){
+    // console.log("hello"); it worked!
+    element.innerText = "Logout";
+}
+
+function removeButton(element){
+    // console.log("removing"); it works!
+    element.remove();
+}
+
+
+
+// I just wanted to see if it would still work if I named the parameter button. It did!
+
+// function logout(button){
+//     // console.log("hello"); it worked!
+//     button.innerText = "Logout";
+// }
