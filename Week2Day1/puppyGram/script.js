b7fe76accdc6d5617455d699d0cbbf967a0df90b
Misty94 <mms.strickland@gmail.com>
2022-06-28 16:00:02 -0400

    Lecture

--- Week2Day1/puppyGram/script.js
+++ Week2Day1/puppyGram/script.js
@@ -30,3 +30,21 @@ function cookieMonster(){
     var element = document.querySelector("#cookie-box");
     element.remove();
 }
+
+function incrementLike(id){
+    console.log("likeing!", id);
+    var element = document.querySelector(id); //this line grabs the element from the html whose id is equal to the variable id
+    // console.log("element looks like this", element.innerText);
+    let likeCount = Number(element.innerText); //element.innerText is the like count inside the span element. We will convert that to a number and store it in a variable.
+    // console.log(likeCount);
+    likeCount += 1; // increment the like count
+    element.innerText = likeCount; // update the element so that its innerText is the new like count
+}
+
+
+
+// in css
+// .like-count::after {
+//     content: "like(s)";
+//      background-color: blue;
+// }
