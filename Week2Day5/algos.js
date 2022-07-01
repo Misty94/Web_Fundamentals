bb351538648b8309ffd1a3aba7641090a9e51696
Misty94 <mms.strickland@gmail.com>
2022-07-01 12:36:27 -0400

    morning algos

---
+++ Week2Day5/algos.js
@@ -0,0 +1,21 @@
+// function sameCase(a,b){
+//     const validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+//     let aIsUpper = false;
+//     let bIsUpper = false;
+    
+//     if(a.toUpperCase() === a){
+//         aIsUpper = true;
+//     }
+//     if(b.toUpperCase() === b){
+//         bIsUpper = true
+//     }
+//     if(aIsUpper === bIsUpper){
+//         return 1;
+//     } 
+//     else if(a in validChars && b in validChars){
+//         return 0;
+//     }
+//     else {
+//         return -1}
+    
+// }
